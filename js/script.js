8d172502d6ba0a0a8074d82081408ef255784f56
DevUchenna <agunnabueze@gmail.com>
2022-05-16 15:34:49 +0100

    first commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 //Choose a random color
-const button = document.querySelector('button')
-const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const button = document.querySelector('button');
+const body = document.querySelector('body');
+const colors = ['gold', 'yellow', 'purple','olive','beige','blue','cyan', 'indigo','lime green'];
 
-body.style.backgroundColor = 'violet'
-button.addEventListener('click', changeBackground)
+body.style.backgroundColor = 'chartreuse';
+button.addEventListener('click', changeBackground);
 
+// function that works when the button is clicked
 function changeBackground(){
-const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+    const colorIndex = parseInt(Math.random()*colors.length);
+    body.style.backgroundColor = colors[colorIndex];
 }
